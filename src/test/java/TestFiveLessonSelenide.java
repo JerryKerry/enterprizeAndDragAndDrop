9119c3f54c8bf4af460f437b882bb65e84859f62
kolomiets.aa <kolomiets.aan@gazprom-neft.ru>
2025-06-27 19:00:58 +0300

    фиксанул пустые строки

--- src/test/java/TestFiveLessonSelenide.java
+++ src/test/java/TestFiveLessonSelenide.java
@@ -1,4 +1,3 @@
-
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -7,8 +6,6 @@ import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selectors.withTagAndText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
-
-
 
 public class TestFiveLessonSelenide {
     @BeforeAll
@@ -23,7 +20,6 @@ public class TestFiveLessonSelenide {
         $(withTagAndText("button", "Solution")).sibling(0).$(withText("Enterprises"))
                 .click();
         $("#hero-section-brand-heading").shouldHave(exactText("The AI-powered\ndeveloper platform"));
-
     }
 
     @Test
@@ -32,10 +28,7 @@ public class TestFiveLessonSelenide {
         actions().moveToElement($("#column-a")).clickAndHold().moveToElement($("#column-b")).
                 release().perform();
         $("#column-a").shouldHave(exactText("B"));
-
-
     }
-
 }
 
 
