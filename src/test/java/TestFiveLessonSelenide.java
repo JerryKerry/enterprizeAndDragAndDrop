09f36e99b6016e2e122c48b217b290829059dff4
kolomiets.aa <kolomiets.aan@gazprom-neft.ru>
2025-06-27 19:25:33 +0300

    поправил название методов, добавл тест с методов dragAndDrop to

--- src/test/java/TestFiveLessonSelenide.java
+++ src/test/java/TestFiveLessonSelenide.java
@@ -3,18 +3,19 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.DragAndDropOptions.to;
 import static com.codeborne.selenide.Selectors.withTagAndText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestFiveLessonSelenide {
     @BeforeAll
-    static void beforeAll(){
+    static void beforeAll() {
         Configuration.browserSize = "1920x1080";
     }
 
     @Test
-    void testTittleAI(){
+    void tittleAITest() {
         open("https://github.com");
         $(withTagAndText("button", "Solution")).hover();
         $(withTagAndText("button", "Solution")).sibling(0).$(withText("Enterprises"))
@@ -23,12 +24,19 @@ public class TestFiveLessonSelenide {
     }
 
     @Test
-    void testDragAndDrop(){
+    void moveToElementTest() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
         actions().moveToElement($("#column-a")).clickAndHold().moveToElement($("#column-b")).
                 release().perform();
         $("#column-a").shouldHave(exactText("B"));
     }
-}
 
+    @Test
+    void dragAndDropTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDrop(to("#column-b"));
+
+        $("#column-a").shouldHave(exactText("B"));
+    }
+}
 
