0c96cd393a24a7a49caee7b9f3efb8542cfdcded
kolomiets.aa <kolomiets.aan@gazprom-neft.ru>
2025-06-30 12:06:52 +0300

    Добавил проверку начального состояния элементов

--- src/test/java/TestFiveLessonSelenide.java
+++ src/test/java/TestFiveLessonSelenide.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.DragAndDropOptions.to;
 import static com.codeborne.selenide.Selectors.withTagAndText;
 import static com.codeborne.selenide.Selectors.withText;
@@ -34,6 +35,8 @@ public class TestFiveLessonSelenide {
     @Test
     void dragAndDropTest() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
         $("#column-a").dragAndDrop(to("#column-b"));
 
         $("#column-a").shouldHave(exactText("B"));
